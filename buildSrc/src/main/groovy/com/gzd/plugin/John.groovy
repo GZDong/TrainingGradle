9672a25a0c86dec37333355df83bdd2884376df8
GZDong <dong_scau@163.com>
2019-12-24 00:58:36 +0800

    plugin

--- buildSrc/src/main/groovy/com/gzd/plugin/John.groovy
+++ buildSrc/src/main/groovy/com/gzd/plugin/John.groovy
@@ -1,5 +1,6 @@
 package com.gzd.plugin
 
+import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,10 +8,13 @@ class John implements Plugin<Project> {
 
     @Override
     void apply(Project target) {
-        def extension  = target.extensions.create("myDefine", MyExtension) //定义成一个方法，默认是修改属性值
+        def extension = target.extensions.create("myDefine", MyExtension) //定义成一个方法，默认是修改属性值
         println("hello ${extension.p1}")
         target.afterEvaluate { //全部的配置跑完后执行
             println("hello ${extension.p1}")
         }
+        def extension1 = target.extensions.getByType(BaseExtension)
+        def testTransform1 = new TestTransform()
+       // extension1.registerTransform(testTransform1)
     }
 }
